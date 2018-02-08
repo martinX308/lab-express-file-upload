1090f9a7f5f4cd5ee277c03231d633aead1e6b35
Martin <mar.gardemann@gmail.com>
2018-02-08 10:19:43 +0100

    upload tested

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const Picture = require('../models/picture');
+
+const upload = multer({ dest: './public/uploads/' });
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+// Route to upload from project base path
+
+router.post('/upload', upload.single('photo'), (req, res, next) => {
+  const pic = new Picture({
+    name: req.body.name,
+    path: `/uploads/${req.file.filename}`,
+    originalName: req.file.originalname
+  });
+
+  pic.save((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
